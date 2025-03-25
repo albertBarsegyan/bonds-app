4e45c16a48dbbc7b32903e30b3f37c30bb9cc374
Your Name <you@example.com>
2025-03-25 17:17:50 +0400

    first commit

---
+++ eslint.config.js
@@ -0,0 +1,39 @@
+import { fixupConfigRules } from "@eslint/compat";
+import pluginJs from "@eslint/js";
+import eslintConfigPrettier from "eslint-plugin-prettier/recommended";
+import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
+    rules: {
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+    },
+  },
+  ...fixupConfigRules(pluginReactConfig),
+  eslintConfigPrettier,
+  {
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    rules: {
+      "react/jsx-uses-react": "off",
+      "react/react-in-jsx-scope": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+  },
+];
